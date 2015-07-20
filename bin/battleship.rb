0700a4f90587219b5fa402ac3b842ae8648a6308
Priya Patil <priya10487@aol.com>
2015-07-20 11:03:10 +0100

    Split display_table method into display_columns and display_rows.

--- bin/battleship.rb
+++ bin/battleship.rb
@@ -5,9 +5,9 @@ $LOAD_PATH.unshift File.dirname(__FILE__) + '/../lib'
 require 'grid'
 require 'curses'
 
-grid = Battleship.new(2,2,["E","E","E","E","E","E","E","E","E"]).place_ship(2)
+grid = Battleship.new(["E","E","E","E","E","E","E","E","E"]).place_ship(2)
 COLUMNS = ['1','2','3']
 ROWS = ['A','B','C']
 
-Grid.new(Kernel,Kernel,ROWS,COLUMNS,3,grid).start
+Grid.new(Kernel,Kernel,ROWS,COLUMNS,grid).start
 

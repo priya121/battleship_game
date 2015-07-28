0ff0d70c479f5b132f8b78bdfc2b7603ad5ea490
Priya Patil <priya10487@aol.com>
2015-07-28 16:35:28 +0100

    Changed bin folder to use Display.draw_ship method instead of Game.place_ship.

--- bin/battleship.rb
+++ bin/battleship.rb
@@ -7,12 +7,9 @@ require 'grid'
 
 COLUMNS = ['1','2','3','4','5']
 ROWS = ['A','B','C','D','E']
+cells = ["E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E"]
 
-grid = Game.new(Kernel,["E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E","E"],ROWS,COLUMNS)
+changed_cells = Display.new(cells,ROWS,COLUMNS,Kernel).draw_ship(:aircraft_carrier,0)
 
-grid.place_ship(1)
-grid.place_destroyer(7)
-final_grid = grid.place_ship(3)
-
-Grid.new(Kernel,Kernel,ROWS,COLUMNS,final_grid).start
+Game.new(Kernel,Kernel,ROWS,COLUMNS,changed_cells).start
 

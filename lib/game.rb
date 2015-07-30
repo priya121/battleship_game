8b1179ee571db91a65ec9565ceac0e8cc99ea91c
Priya Patil <priya10487@aol.com>
2015-07-30 14:24:05 +0100

    Moved display hit_or_miss and other outputs related to it to the Display class.

--- lib/game.rb
+++ lib/game.rb
@@ -20,41 +20,9 @@ class Game
     @index_after_player_move = @player.give_coordinate
     grid_after_player_move = @grid.target(@index_after_player_move)
     @display.display_table(grid_after_player_move)
-    display_hit_or_miss(grid_after_player_move)
+    @display.display_hit_or_miss(grid_after_player_move,@index_after_player_move)
   end
 
-  def display_hit_or_miss(grid_after_player_move)
-    missed_shot(grid_after_player_move)
-    hit_a_ship(grid_after_player_move)
-    hit_a_destroyer(grid_after_player_move)
-    destroyer_sunk(grid_after_player_move)
-  end
-
-  def missed_shot(grid_after_player_move)
-    unless grid_after_player_move[@index_after_player_move] == "HIT" || grid_after_player_move[@index_after_player_move] == "HD" || grid_after_player_move[@index_after_player_move] =="HS"
-      @output.puts "You missed."
-      @output.puts "Guess again"
-    end
-  end
-
-  def hit_a_ship(grid_after_player_move)
-    if grid_after_player_move.to_s.include?("HIT") 
-      @output.puts "You sunk #{grid_after_player_move.count("HIT")} battleships."
-    else
-    end
-  end
-
-  def hit_a_destroyer(grid_after_player_move)
-    if grid_after_player_move.to_s.include?("HD")
-      @output.puts "You hit #{grid_after_player_move.count("HD")} part of a destroyer."
-    end
-  end
-
-  def destroyer_sunk(grid_after_player_move)
-    if grid_after_player_move.count("HD") == 2
-      @output.puts "You sunk a destroyer."
-    end
-  end
 
   def start
     while ships_left_on_grid == true
--- lib/score.rb
+++ lib/score.rb
@@ -37,3 +37,4 @@ class Score
   end
 
 end
+

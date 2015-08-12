c0ba7ae4d43fbf2d95f4e3d95b1ccc8d0087f379
Priya Patil <priya10487@aol.com>
2015-08-12 14:13:25 +0100

    Changed ships_left_on_grid class method in Game class to no longer use the score class.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,10 @@
-require 'score'
-
 class Game
+  SHIPS = {:battleship => 4,
+    :destroyer => 2,
+    :submarine => 3,
+    :aircraft_carrier => 5,
+    :cruiser => 3}
+
   def initialize(input,output,rows,columns,cells)
     @input = input
     @output = output
@@ -10,7 +14,6 @@ class Game
     @player = Player.new(@input,@output,@row_label,@column_label)
     @grid = Grid.new(@input,@cells,@row_label,@column_label) 
     @display = Display.new(@cells,@row_label,@column_label,@output)
-    @score = Score.new(@cells)
   end
 
   def ask_player_guess
@@ -30,7 +33,7 @@ class Game
     end
 
   def ships_left_on_grid?
-    @score.ships_left_on_grid?
+    @cells.any? { |c|
+      SHIPS.keys.include?(c) }
   end
-
 end
